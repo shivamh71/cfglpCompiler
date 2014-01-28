24d8d5722d9b0728ace80c61acc6887b9db2d473
shivamh71 <shivamh71@gmail.com>
2014-01-28 14:13:11 +0530

    updated parser.yy new_parser.yy

--- cfglp0-src-int/level-0-64bits/test_files/test.c
+++ cfglp0-src-int/level-0-64bits/test_files/test.c
@@ -1,8 +1,18 @@
 int a;
 main()
 {
-	int b = 5;
-	int c = 6;
-	c = b + b;
+	int b=3;
+	int c=1;
+	int d = 10;
+	b = (c = 2);
+	if (c!=2) {
+
+	}
+	else {
+		for (;;);
+	}
+	while (c!=b) {
+		d = c; 
+	}
 	return;
 }
