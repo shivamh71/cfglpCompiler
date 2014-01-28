eb4a3c829a9a38a283ab4a952521d8705c979873
shivamh71 <shivamh71@gmail.com>
2014-01-28 19:24:40 +0530

    finally commited

--- cfglp0-src-int/level-1-64bits/test_files/test.c
+++ cfglp0-src-int/level-1-64bits/test_files/test.c
@@ -3,15 +3,9 @@ main()
 	int a = 4;
 	int b = 3;
 	int c = 3;
-<<<<<<< HEAD
 	int d ;
 	d = ((a == b) != c);
 	if (c > (a > b)?0:21)
-=======
-	int d = (a<=(b>c));
-
-	if (c > (a > b))
->>>>>>> b527e032f1fff214ffd4936a257539beb52d1610
 		a = 6;
 	else
 		b = 6;
